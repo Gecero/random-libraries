7a3f8bbf57c9512c11ecd6e87bdf1bca68bd83bb
Gecero <27288418+Gecero@users.noreply.github.com>
2018-10-03 20:19:45 +0200

    updated constmath.hpp
    - added to-do list
    - now only supports c++17
    - now uses lambdas
    - more functions
    - now only static inline auto functions
    - more compiler macros. fun for the whole family!
    - nice code formatting (when using tab size 4)

--- constmath.hpp
+++ constmath.hpp
@@ -1,18 +1,23 @@
-#ifndef CONSTMATH_HPP
+// TO-DO:
+// - fully add <cmath> (http://www.cplusplus.com/reference/cmath/)
+// - fully add <complex> (http://www.cplusplus.com/reference/complex/)
+// - prototypes
+// - more type compability
+
+#if !defined(__cplusplus) || !__cplusplus > 201402L
+	#error this library needs c++17
+#elif !defined(CONSTMATH_HPP)
 #define CONSTMATH_HPP
 namespace gcr {
-	constexpr bool isEven(uint64_t number);
-	constexpr bool isOdd(uint64_t number);
-	constexpr uint64_t ceil(long double number);
-	constexpr uint64_t floor(long double number);
-	constexpr long double sin(long double number);
-
-
-
-	constexpr bool isEven(uint64_t number) { return ((number + 1) & 1); }
-	constexpr bool isOdd(uint64_t number) { return !((number + 1) & 1); }
-	constexpr uint64_t ceil(long double number) { return (uint64_t)(number + 0.5); }
-	constexpr uint64_t floor(long double number) { return (uint64_t)(number); }
-	constexpr long double sin(long double number) { return 20.785*((number*0.15915) - (int)(number*0.15915))*(((number*0.15915) - (int)(number*0.15915)) - 0.5)*(((number*0.15915) - (int)(number*0.15915)) - 1.0); }
+	static inline auto ceil =		[](long double a)									constexpr -> uint64_t		{ return (uint64_t)(a); };
+	static inline auto floor =		[](long double a)									constexpr -> uint64_t		{ return (uint64_t)(a + 0.5); };
+	static inline auto sin =		[](long double a)									constexpr -> long double	{ return 20.785*((a*0.15915) - (int)(a*0.15915))*(((a*0.15915) - (int)(a*0.15915)) - 0.5)*(((a*0.15915) - (int)(a*0.15915)) - 1.0); };
+	static inline auto fmin =		[](long double a, long double b)					constexpr -> long double	{ return a > b ? b : a; };
+	static inline auto fmax =		[](long double a, long double b)					constexpr -> long double	{ return a < b ? b : a; };
+	static inline auto fdim =		[](long double a, long double b)					constexpr -> long double	{ return a > b ? a - b : 0; };
+	static inline auto fabs =		[](long double a)									constexpr -> long double	{ return a < 0 ? -a : a; };
+	static inline auto abs =		[](long double a)									constexpr -> long double	{ return a < 0 ? -a : a; };
+	static inline auto fma =		[](long double a, long double b, long double c)		constexpr -> long double	{ return a * b + c; };
+	static inline auto pow =		[](long double a, uint64_t b)						constexpr -> long double	{ long double out = 1.0; for (uint64_t i = 0; i < b; i++) { out *= a; } return out; }; // 10.57x faster than normal pow
 }
 #endif
