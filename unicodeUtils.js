417d05cc5f40bde572f01ce9119d7001be37a351
Gecero <27288418+Gecero@users.noreply.github.com>
2018-10-06 12:40:18 +0200

    updated unicodeUtils.js
    - added a toSmallCaps function

--- unicodeUtils.js
+++ unicodeUtils.js
@@ -18,3 +18,10 @@ function toFullWidth(text) {
 	}
 	return text;
 }
+
+function toSmallCaps(text) {
+	for(var i = 0; i < text.length; i++) {
+		text = text.replace("a", "ᴀ").replace("b", "ʙ").replace("c", "ᴄ").replace("d", "ᴅ").replace("e", "ᴇ").replace("f", "ғ").replace("g", "ɢ").replace("h", "ʜ").replace("i", "ɪ").replace("j", "ᴊ").replace("k", "ᴋ").replace("l", "ʟ").replace("m", "ᴍ").replace("n", "ɴ").replace("o", "ᴏ").replace("p", "ᴘ").replace("q", "ǫ").replace("r", "ʀ").replace("s", "s").replace("t", "ᴛ").replace("u", "ᴜ").replace("v", "ᴠ").replace("w", "ᴡ").replace("x", "x").replace("y", "ʏ").replace("z", "ᴢ");
+	}
+	return text;
+}
